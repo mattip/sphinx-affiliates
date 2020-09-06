274766e564168f0cb1dba9f5951c3e8266a7f273
mattip <matti.picus@gmail.com>
2020-09-06 20:08:35 +0300

    fix flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,14 @@
-import os.path
-import pathlib
-import shutil
-
 import pytest
 from sphinx.testing.path import path
 
 pytest_plugins = 'sphinx.testing.fixtures'
 
+
 @pytest.fixture(scope='session')
 def rootdir():
     return path(__file__).parent.abspath() / 'docs'
 
+
 def pytest_configure(config):
-    config.addinivalue_line("markers", "sphinx: should have been added upstream")
+    config.addinivalue_line("markers",
+                            "sphinx: should have been added upstream")
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,8 +1,9 @@
 import os
 import pytest
 
+
 @pytest.mark.sphinx('html', testroot='basic/')
-def test_build_html(app, status, warning):  
+def test_build_html(app, status, warning):
     app.builder.build_all()
     build = os.path.join(app.env.srcdir, '_build', 'html')
     with open(os.path.join(build, 'search.html')) as fid:
@@ -11,6 +12,6 @@ def test_build_html(app, status, warning):
             if 'affiliate_searchindex' in line:
                 lines.append(line)
         assert len(lines) == 2
-        
+
     files = os.listdir(build)
     assert 'affiliate_searchindex.js' in files
